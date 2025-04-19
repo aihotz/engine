7ad0ffb0adadb0ad07decf51c0846f80305554a7
aihotz <aihotz_arruti_aldama@outlook.es>
2025-04-19 19:09:09 +0200

    WIP game as both library and standalone (not compiling!)

--- projects/proof/src/main.cpp
+++ projects/proof/src/main.cpp
@@ -1,6 +1,21 @@
 #include "logger.hpp"
+#include "core.hpp"
 
+void Initialize()
+{
+    shared::Log("Game code initialization!");
+}
+
+#ifdef DLL_BUILD
+extern "C" __declspec(dllexport) void DLLEndpointInitialize()
+{
+    shared::Log("DLL endpoint!");
+    Initialize();
+}
+#else
 int main(void)
 {
-    shared::Log("Game code!");
+    shared::Log("Standalone executable endpoint!");
+    Initialize();
 }
+#endif
